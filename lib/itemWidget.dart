020ff718f14c30fef185f619c96463c38840c22f
nitrojenfaruk <92308256+nitrojenfaruk@users.noreply.github.com>
2022-06-21 02:39:37 +0300

    Update itemWidget.dart

--- lib/itemWidget.dart
+++ lib/itemWidget.dart
@@ -9,51 +9,67 @@ class ItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.all(5.0),
       child: Card(
-        elevation: 0,
+        elevation: 1,
         color: Colors.white,
         child: Padding(
-          padding: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.all(0.0),
           child: ListTile(
-            leading: Image.network(
+            onTap: () {
+              autoFill(item.name);
+            },
+            leading: Container(
+              width: width / 4,
+              height: width,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: NetworkImage(
+                      item.image,
+                    )),
+                borderRadius: const BorderRadius.all(Radius.circular(8.0)),
+                color: Colors.teal.shade200,
+              ),
+            ),
+            /*Image.network(
               item.image,
               height: 90,
               width: 90,
-            ),
+            ),*/
             title: Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.all(4.0),
               child: Center(
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                    textStyle: const TextStyle(fontSize: 20),
+                child: Text(
+                  item.name,
+                  style: TextStyle(
+                    color: Colors.indigo.shade900,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
                   ),
-                  onPressed: () {
-                    autoFill(item.name);
-                  },
-                  child: Text(item.name),
                 ), /*Text(item.name,
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18))*/
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18))*/
               ),
             ),
             subtitle: Center(
                 child: Text(item.desc,
                     style: TextStyle(
-                        color: Colors.indigo.shade500,
+                        color: Colors.deepOrange,
                         fontWeight: FontWeight.bold,
                         fontSize:
-                            15))), /*
-            trailing: Text(
-              "\$${item.price}",
-              style: TextStyle(
-                  color: Colors.purple,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20),
-            ),*/
+                            14))), /*
+              trailing: Text(
+                "\$${item.price}",
+                style: TextStyle(
+                    color: Colors.purple,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20),
+              ),*/
           ),
         ),
       ),

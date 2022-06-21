b0764decadb4dcde208bb5f2f58ed1a15c272c77
Mustafa Karakaş <mustafa.krks.09@gmail.com>
2022-06-21 21:50:18 +0300

    Update catalog.dart

--- lib/catalog.dart
+++ lib/catalog.dart
@@ -106,12 +106,13 @@ class Item {
   final num price;
   final String color;
   final String image;
+
   Item(
       {required this.id,
       required this.name,
       required this.desc,
       required this.price,
       required this.color,
-      required this.image}
-  );
+      required this.image});
 }
+
